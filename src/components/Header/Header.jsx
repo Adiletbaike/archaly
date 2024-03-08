1127d390b6723c83854ab0efd63cca75fefa0cf6
Adiletbaike <adiletdzhuraev@gmail.com>
2024-03-08 18:07:40 +0600

    Some changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="bg-green-100 body-font">
       <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-1 md:mb-0">
-          <img src="/public/logo.png" width="60" alt="" />
+          <img src="../public/logo.png" width="60" alt="" />
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
           <a className="mr-5 hover:text-gray-900">Башкы бет</a>
