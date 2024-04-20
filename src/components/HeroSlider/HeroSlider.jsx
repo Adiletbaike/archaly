f2adda24fae2b61968b9e110610ece3e0d508e2b
Adiletbaike <adiletdzhuraev@gmail.com>
2024-04-20 18:29:58 +0600

    HeroSlider

--- src/components/HeroSlider/HeroSlider.jsx
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -17,8 +17,8 @@ const HeroSlider = ({ slides, hadith }) => {
 
   useEffect(() => {
     const imageInterval = setInterval(() => {
-      nextSlide();
-    }, 5000);
+      nextSlide(); 
+    }, 5000); 
     return () => clearInterval(imageInterval);
   }, [currentSlide, slides.length]);
 
@@ -26,7 +26,7 @@ const HeroSlider = ({ slides, hadith }) => {
     const hadithInterval = setInterval(() => {
       const newIndex = (currentHadithIndex + 1) % hadith.length;
       setCurrentHadithIndex(newIndex);
-    }, 4000);
+    }, 4000); 
     return () => clearInterval(hadithInterval);
   }, [currentHadithIndex, hadith.length]);
 
@@ -36,19 +36,19 @@ const HeroSlider = ({ slides, hadith }) => {
   return (
     <section className="relative h-[600px] md:h-screen overflow-hidden">
       <div
-        className="absolute inset-0 w-full h-full transition-opacity duration-1000 opacity-90 bg-cover bg-center text-gray-900"
+        className="absolute inset-0 w-full h-full duration-1000 opacity-90 bg-cover bg-center text-gray-900"
         style={{
           backgroundImage: `url(${currentSlideData.image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-        <div className="container mx-auto h-full flex items-center justify-center">
+        <div className="container mx-auto w-1/2 h-full flex items-center justify-center">
           <div className="text-center">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-hero text-green-800 leading-tight mb-24">
+            {/* <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-hero text-green-800 leading-tight mb-24">
               {currentSlideData.title}
-            </h1>
-            <p className="bg-white bg-opacity-50 p-2 rounded-lg text-xl sm:text-2xl md:text-3xl text-green-800 font-subHero leading-relaxed mb-2">
+            </h1> */}
+            <p className="bg-white bg-opacity-50 p-2 rounded-lg sm:text-xl md:text-3xl text-green-800 font-subHero leading-relaxed mb-2 duration-1000 opacity-90">
               {currentHadith}
             </p>
           </div>
@@ -58,13 +58,13 @@ const HeroSlider = ({ slides, hadith }) => {
         className="absolute top-1/2 transform -translate-y-1/2 left-4 text-white text-xl bg-green-700 bg-opacity-50 px-4 py-2 rounded-lg"
         onClick={prevSlide}
       >
-        <img src="/images/arrow-prev.png" alt="" style={{ width: "20px" }} />
+        <img src="/images/arrow-prev.png" alt="" style={{ width: '20px' }} />
       </button>
       <button
         className="absolute top-1/2 transform -translate-y-1/2 right-4 text-white text-xl bg-green-700 bg-opacity-50 px-4 py-2 rounded-lg"
         onClick={nextSlide}
       >
-        <img src="/images/arrow-next.png" alt="" style={{ width: "20px" }} />
+        <img src="/images/arrow-next.png" alt="" style={{ width: '20px' }} />
       </button>
     </section>
   );
