bfa5f4bc78006b2ed54ef87e54420accbc05522a
Adiletbaike <adiletdzhuraev@gmail.com>
2024-04-20 18:00:59 +0600

    HeroSlider

--- src/components/HeroSlider/HeroSlider.jsx
+++ src/components/HeroSlider/HeroSlider.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-const HeroSlider = ({ slides }) => {
+const HeroSlider = ({ slides, hadith }) => {
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentHadithIndex, setCurrentHadithIndex] = useState(0);
 
+  // Slayt geçişleri için ileri ve geri yönlü fonksiyonlar
   const nextSlide = () => {
     const newIndex = (currentSlide + 1) % slides.length;
     setCurrentSlide(newIndex);
@@ -14,50 +16,55 @@ const HeroSlider = ({ slides }) => {
   };
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      nextSlide(); // Otomatik olarak bir sonraki slayta geç
-    }, 5000); // Her 5 saniyede bir slaytı değiştir
-
-    return () => clearInterval(interval);
+    const imageInterval = setInterval(() => {
+      nextSlide();
+    }, 5000);
+    return () => clearInterval(imageInterval);
   }, [currentSlide, slides.length]);
+
+  useEffect(() => {
+    const hadithInterval = setInterval(() => {
+      const newIndex = (currentHadithIndex + 1) % hadith.length;
+      setCurrentHadithIndex(newIndex);
+    }, 4000);
+    return () => clearInterval(hadithInterval);
+  }, [currentHadithIndex, hadith.length]);
+
+  const currentSlideData = slides[currentSlide];
+  const currentHadith = hadith[currentHadithIndex];
 
   return (
     <section className="relative h-[600px] md:h-screen overflow-hidden">
-      {slides.map((slide, index) => (
-        <div
-          key={index}
-          className={`absolute inset-0 w-full h-full transition-opacity duration-1000 ${
-            index === currentSlide ? "opacity-90" : "opacity-0"
-          } bg-cover bg-center text-gray-900`}
-          style={{
-            backgroundImage: `url(${slide.image})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="container mx-auto h-full flex items-center justify-center">
-            <div className="text-center">
-              <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-hero text-green-800 leading-tight mb-24">
-                {slide.title}
-              </h1>
-              <p className="bg-white bg-opacity-50 p-2 rounded-lg text-xl sm:text-2xl md:text-3xl text-green-800 font-subHero leading-relaxed mb-2">
-                {slide.description}
-              </p>
-            </div>
+      <div
+        className="absolute inset-0 w-full h-full transition-opacity duration-1000 opacity-90 bg-cover bg-center text-gray-900"
+        style={{
+          backgroundImage: `url(${currentSlideData.image})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="container mx-auto h-full flex items-center justify-center">
+          <div className="text-center">
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold font-hero text-green-800 leading-tight mb-24">
+              {currentSlideData.title}
+            </h1>
+            <p className="bg-white bg-opacity-50 p-2 rounded-lg text-xl sm:text-2xl md:text-3xl text-green-800 font-subHero leading-relaxed mb-2">
+              {currentHadith}
+            </p>
           </div>
         </div>
-      ))}
+      </div>
       <button
         className="absolute top-1/2 transform -translate-y-1/2 left-4 text-white text-xl bg-green-700 bg-opacity-50 px-4 py-2 rounded-lg"
         onClick={prevSlide}
       >
-        <img src="/images/arrow-prev.png" alt="" style={{width: '20px'}} />
+        <img src="/images/arrow-prev.png" alt="" style={{ width: "20px" }} />
       </button>
       <button
         className="absolute top-1/2 transform -translate-y-1/2 right-4 text-white text-xl bg-green-700 bg-opacity-50 px-4 py-2 rounded-lg"
         onClick={nextSlide}
       >
-        <img src="/images/arrow-next.png" alt="" style={{width: '20px'}} />
+        <img src="/images/arrow-next.png" alt="" style={{ width: "20px" }} />
       </button>
     </section>
   );
